6ffb55c44dfdaddf448ee68ee389cef2b85d1870
Thiago Sam <thiagosamthi@gmail.com>
2024-08-27 10:52:01 -0400

    simplificação de código javascript

--- main.js
+++ main.js
@@ -1,40 +1,18 @@
 const form = document.getElementById('form-agenda')
 const inputNomeContato = document.getElementById('nome-contato')
 const inputNumeroContato = document.getElementById('numero-contato')
-const nomes = []
-const numero = []
-let linhas = ''
+const tabelaContatos = document.getElementById('tabela-contatos')
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', function(e) {
     e.preventDefault()
 
-    adicionarLinha()
-    atualizarTabela()
+    let novoContato = tabelaContatos.insertRow()
+    let novoNome = novoContato.insertCell()
+    let novoNumero = novoContato.insertCell()
 
-})
-
-function adicionarLinha() {
-
-    if(nomes.includes(inputNomeContato.value)) {
-        alert(`O nome: ${inputNomeContato.value} já existe`)
-    } else {
-        nomes.push(inputNomeContato.value)
-        numero.push(inputNumeroContato.value)
-
-        let linha = '<tr>'
-        linha += `<td>${inputNomeContato.value}</td>`
-        linha += `<td>${inputNumeroContato.value}</td>`
-        linha += '</tr>'
-
-        linhas += linha
-
-    }
+    novoNome.textContent = inputNomeContato.value
+    novoNumero.textContent = inputNumeroContato.value
 
     inputNomeContato.value = ''
     inputNumeroContato.value = ''
-}
-
-function atualizarTabela() {
-    const corpoTabela = document.querySelector('tbody')
-    corpoTabela.innerHTML = linhas
-}
+})
